b5a01e4348947bb3d0a97f621df935877d5f97e8
Swagat Rokade <Swagat.Rokade@wolterskluwer.com>
2021-04-19 13:08:54 +0530

    DemoBaseContent

---
+++ js/file.js
@@ -0,0 +1,8 @@
+﻿//$(document).ready(function () {
+//    // click to scroll top
+//    $('.move-up span').click(function () {
+//        $('html, body').animate({
+//            scrollTop: 0
+//        }, 1000);
+//    });
+//});
